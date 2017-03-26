9a2d668bca7edadc294dd9693e37000292721fc9
Senshee <oooevolution@gmail.com>
2017-03-26 19:54:58 +0200

    Entete PHP pour test
    entete des pages "contenu" introduite en php pour tester sur le serveur
    de Monsieur Maxime Callet.

--- callofduty.php
+++ callofduty.php
@@ -9,18 +9,8 @@
 
 	<body>
 		<div id="bloc_page">
-			<header class="ban">	
-				<a href="index.html"><img class="logoheader" src="image/logo.png" alt="logo du site" />
-				<div class="titresite">
-					<h1>Maxime callet</h1>
-					<div class="alignh2">
-						<div class="bordureh2l"></div>
-						<h2>Unity Developer</h2>
-						<div class="bordureh2r"></div>
-					</div>
-					</a>
-				</div>
-			</header>
+		
+			<?php include("entete.php"); ?>
 
 			<section class="home">
 				<header class="headhome2">
