900c001891956652a5013f1371d4e2c84de111e6
mcallet <maxime.callet@me.com>
2017-03-27 15:49:15 +0200

    Ajout numéro de téléphone et mail sur le footer

--- footer.php
+++ footer.php
@@ -1,15 +1,22 @@
 <footer>
 	<div class="footerlogosite">
-		<img class="logoheader" src="image/logo.png" alt="logo du site" />
+		<img class="logoheader" src="image/logomaxime.svg" alt="logo du site" />
 		<div class="titresite">
 			<h1>Maxime callet</h1>
 			<div class="alignh2">
 				<div class="bordureh2l"></div>
 				<h2>Unity Developer</h2>
-				<div class="bordureh2r"></div>	
+				<div class="bordureh2r"></div>
 			</div>
+			<h3>
+				06 61 28 67 07 <br />
+				maxime.callet@me.com
+			</h3>
+			<p>
+				Design : Maxime Callet <br />
+				Dév. et intégration : Thomas Meytre
+			</p>
+			<p></p>
 		</div>
-		<p>Design : Maxime Callet <br />
-		Dév. et intégration : Thomas Meytre</p>
 	</div>
 </footer>
