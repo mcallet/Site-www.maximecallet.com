42ee8d76e9a0760eaabd778afebd791c35334f0b
mcallet <maxime.callet@me.com>
2017-03-30 18:05:30 +0200

    Footer : Formulaire de contact

--- footer.php
+++ footer.php
@@ -1,22 +1,39 @@
 <footer>
-	<div class="footerlogosite">
-		<img class="logoheader" src="image/logomaxime.svg" alt="logo du site" />
-		<div class="titresite">
-			<h1>Maxime callet</h1>
-			<div class="alignh2">
-				<div class="bordureh2l"></div>
-				<h2>Unity Developer</h2>
-				<div class="bordureh2r"></div>
+	<div class="blockfooter">
+		<div class="bl">
+			<div class="footerlogosite">
+				<img class="logoheader" src="image/logomaxime.svg" alt="logo du site" />
+				<div class="titresite">
+					<h1>Maxime callet</h1>
+					<div class="alignh2">
+						<div class="bordureh2l"></div>
+						<h2>Unity Developer</h2>
+						<div class="bordureh2r"></div>
+					</div>
+					<h3>
+						06 61 28 67 07 <br />
+						maxime.callet@me.com
+					</h3>
+					<p>
+						Design : Maxime Callet <br />
+						Dév. et intégration : Thomas Meytre
+					</p>
+					<p></p>
+				</div>
 			</div>
-			<h3>
-				06 61 28 67 07 <br />
-				maxime.callet@me.com
-			</h3>
-			<p>
-				Design : Maxime Callet <br />
-				Dév. et intégration : Thomas Meytre
-			</p>
-			<p></p>
+		</div>
+		<div class="bl">
+			<div class="inputcontact">
+				<form method="post" action="form.php">
+					<p>
+						<input type="text" name="email" placeholder="Email..." id="email" />
+					</p>
+					<p>
+						<textarea name="message" placeholder="Entrez votre message..." id="message" ></textarea>
+					</p>
+					<input type="submit" name="" "envoi" value="Envoyer" />
+				</form>
+			</div>
 		</div>
 	</div>
 </footer>
